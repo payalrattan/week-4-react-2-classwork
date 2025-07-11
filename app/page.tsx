b17e9e69fa02e4c47af7e63c64f57566673e0593
Payal Rattan <payalrattan21@gmail.com>
2025-07-11 09:18:45 +0200

    update files

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { AstronomyPOD } from "@/components/astronomyPOD/AstronomyPOD";
 import { RoverPhotos } from "@/components/roverPhotos/RoverPhotos";
+
+
 export default function Home() {
   return (
     <div>
       <AstronomyPOD />
       <RoverPhotos />
+ 
     </div>
   );
 }
