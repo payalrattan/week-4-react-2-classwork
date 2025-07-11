0423d567f62a6567af8e40b5c70cc2acb0834014
Payal Rattan <payalrattan21@gmail.com>
2025-07-11 09:20:24 +0200

    modified:   app/page.tsx

--- components/roverPhotos/RoverPhotos.tsx
+++ components/roverPhotos/RoverPhotos.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { NASA_URLs } from "@/data/nasaApi/nasaApiUrls";
@@ -6,18 +5,15 @@ import { Image } from "@/components/image/Image";
 import { useEffect, useState } from "react";
 import styles from "./roverPhotos.module.css";
 
-
-
 export function RoverPhotos() {
   const [marsRover, setMarsRover] = useState({});
   const title = "Mars Rover Photos";
-
+  async function fetchRoverData() {
+    const response = await fetch(NASA_URLs.marsRoverPhoto);
+    const data = await response.json();
+    setMarsRover(data);
+  }
   useEffect(() => {
-    async function fetchRoverData() {
-      const response = await fetch(NASA_URLs.marsRoverPhoto);
-      const data = await response.json();
-      setMarsRover(data);
-    }
     fetchRoverData();
   }, []);
 
@@ -25,16 +21,27 @@ export function RoverPhotos() {
     <div className={styles.roverContainer}>
       <h1 className={styles.roverTitle}>{title}</h1>
       <div className={styles.roverGrid}>
-        {marsRover.photos && marsRover.photos.map((photo) => (
-          <div key={photo.id} className={styles.roverCard}>
-            <Image imgURL={photo.img_src} alt={photo.name} />
-            <p><b>Rover Id:</b> {photo.id}</p>
-            <p><b>Landing date:</b> {photo.rover.landing_date}</p>
-            <p><b>Launch date:</b> {photo.rover.launch_date}</p>
-            <p><b>Rover Name:</b> {photo.rover.name}</p>
-            <p><b>Status:</b> {photo.rover.status}</p>
-          </div>
-        ))}
+        {marsRover.photos &&
+          marsRover.photos.map((photo) => (
+            <div key={photo.id} className={styles.roverCard}>
+              <Image imgURL={photo.img_src} alt={photo.name} />
+              <p>
+                <b>Rover Id:</b> {photo.id}
+              </p>
+              <p>
+                <b>Landing date:</b> {photo.rover.landing_date}
+              </p>
+              <p>
+                <b>Launch date:</b> {photo.rover.launch_date}
+              </p>
+              <p>
+                <b>Rover Name:</b> {photo.rover.name}
+              </p>
+              <p>
+                <b>Status:</b> {photo.rover.status}
+              </p>
+            </div>
+          ))}
       </div>
     </div>
   );
