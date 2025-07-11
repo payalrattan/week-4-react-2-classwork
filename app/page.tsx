511e8cf2fab3caf06ab2f0dbbb9e3aa295dd5aea
Payal Rattan <payalrattan21@gmail.com>
2025-07-11 10:37:00 +0200

    week-4-react-2-classwork

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import { AstronomyPOD } from "@/components/astronomyPOD/AstronomyPOD";
 import { RoverPhotos } from "@/components/roverPhotos/RoverPhotos";
-
-
 export default function Home() {
   return (
     <div>
       <AstronomyPOD />
       <RoverPhotos />
- 
     </div>
   );
 }
